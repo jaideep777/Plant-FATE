2345fcf1271a27c0a0af5f6ca347c49eda66ae2f
apoorva1720 <99265266+apoorva1720@users.noreply.github.com>
2022-06-11 17:28:14 +0530

    added vasr

--- tests/fitness_test_var.cpp
+++ tests/fitness_test_var.cpp
@@ -23,12 +23,12 @@ public:
 int main(){
 	
 	// Creating random number generator for soil water potential
-	ofstream f_mean("fitness_v_swpmean.txt");
+	ofstream f_var("fitness_v_swpvar.txt");
 	
 	// SET SOIL WATER POTENTIAL SERIES PARAMETERS
-	double swp_mean_start = -0.1;
-	double swp_mean_stop = -10.0;
-	double swp_stddev = -4.0;
+	double swp_var_start = -0.1;
+	double swp_var_stop = -10.0;
+	double swp_mean = -4.0;
 	double time_step_swp = 10;
 	
 	// SET SIMULATION PARAMETERS
@@ -36,16 +36,16 @@ int main(){
 	double sim_end_t = 2100;
 	double sim_dt = 0.1;
 	
-	std::vector<double> swp_mean;
+	std::vector<double> swp_var;
 	
-	f_mean << "SWP_Mean" << "\t"
+	f_mean << "SWP_Var" << "\t"
 	       << "Fitness_Avg" << "\n";
 	
 	
-	for (double i_mean = swp_mean_start; i_mean >= swp_mean_stop; i_mean = i_mean - 1 ){
+	for (double i_var = swp_var_start; i_var>= swp_var_stop; i_var = i_var - 1 ){
 		
 		std::vector<double> germi_avg;
-		cout << "Starting SWP mean : " << to_string(i_mean) << "\n";
+		cout << "Starting SWP var : " << to_string(i_var) << "\n";
 		
 		
 		// STARTING SIMULATION FOR 100 CYCLES FOR AVERAGING
@@ -63,7 +63,7 @@ int main(){
 			C.co2File = "tests/data/CO2_AMB_AmzFACE2000_2100.csv";
 			
 			std::default_random_engine generator;
-			std::normal_distribution<double> dist(i_mean, swp_stddev);
+			std::normal_distribution<double> dist(swp_mean, i_var);
 			for (double t=sim_start_t; t<=sim_end_t; t=t+time_step_swp){
 				C.t_met.push_back(t);
 				double val = dist(generator);
@@ -87,11 +87,11 @@ int main(){
 	
 		double average = accumulate(germi_avg.begin(), germi_avg.end(), 0.0) / germi_avg.size();
 		
-		f_mean << i_mean << "\t"
+		f_mean << i_var << "\t"
 		<< average << "\n";
 		germi_avg.clear();
 	}
-	f_mean.close();
+	f_var.close();
 	return 0;
 }
 
